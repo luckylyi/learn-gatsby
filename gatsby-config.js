c658b0b58e8f8c906288f5a1c1b7d3e8edc3ebde
Lucky Yi <lyi@xogrp.com>
2024-01-25 18:56:26 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `LearnGatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
